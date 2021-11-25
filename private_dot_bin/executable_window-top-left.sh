dea167de8833394ace609bedf070684a927abb3f
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-11-24 20:05:16 -0800

    Updated kb shortcuts

--- private_dot_bin/executable_window-top-left.sh
+++ private_dot_bin/executable_window-top-left.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-wmctrl -r :ACTIVE: -b remove,maximized_vert
-wmctrl -r :ACTIVE: -b remove,maximized_horz
 WIN=$(xdotool getwindowfocus)
 xdotool windowsize "${WIN}" 30% 50%
 xdotool windowmove "${WIN}" 0% 0%
